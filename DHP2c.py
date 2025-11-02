3e987c880a50df8225909bb74f1f4b98f36d48ce
Daniel Hernandez <143033040+dh21704@users.noreply.github.com>
2025-11-01 22:18:46 -0700

    Update DHP2c.py

--- DHP2c.py
+++ DHP2c.py
@@ -2,23 +2,19 @@ from socket import *
 import random
 import time
 
-# Create UDP socket
+#create UDP socket
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('', 12000))
 
 print("UDP Ping Server with delay and loss is ready to receive")
 
 while True:
-    # Receive message from client
-    message, address = serverSocket.recvfrom(1024)
+    message, address = serverSocket.recvfrom(1024) #create message from socket
+    delay = random.uniform(0.01, 0.02) #simulate a 10-20second delay
+    time.sleep(delay) #add delay
 
-    # Simulate 10–20 ms delay
-    delay = random.uniform(0.01, 0.02)
-    time.sleep(delay)
-
-    # Simulate 10% packet loss
+    #if 10% packe tloss
     if random.random() < 0.1:
-        continue  # Skip sending the reply
+        continue #skip sending the reply
 
-    # Send the message back
-    serverSocket.sendto(message, address)
+    serverSocket.sendto(message, address)#send message back
